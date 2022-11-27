e9b8c3991454fea3f18c61ec316bda668d9d1d13
Amor-Jansen <Amor.jansen2@gmail.com>
2022-11-27 13:56:15 +0000

    Add player image in javascript

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById('can1');
 const cxt = canvas.getContext('2d');
-
-const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+const playerImage = new Image();
+playerImage.src = 'assets/images/Walk-rat.png'
