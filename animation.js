31ec23938ff5f9b5a002925a38887561096b5595
Amor-Jansen <Amor.jansen2@gmail.com>
2022-11-27 16:36:57 +0000

    Fix player image to load

--- animation.js
+++ animation.js
@@ -4,13 +4,13 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = './assets/images/player.png'
-const spriteWidth = 575;
-const spriteHeight = 523;
+playerImage.src = 'assets/images/player.png'
+const spriteWidth = 100;
+const spriteHeight = 91;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-   // ctx.fillRect(50,50,100,100);
+    //ctx.fillRect(50,50,100,100);
     ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, CANVAS_WIDTH, CANVAS_HEIGHT);
     requestAnimationFrame(animate);
 };
