8e657628b6b2f8be892d368c44dbeaf5aa63b517
Amor-Jansen <Amor.jansen2@gmail.com>
2022-11-29 13:45:59 +0000

    Add controls to html page

--- animation.js
+++ animation.js
@@ -10,6 +10,8 @@ const playerImage = new Image();
 playerImage.src = 'assets/images/player.png'
 const spriteWidth = 100;
 const spriteHeight = 91;
+let playerState = 'run';
+
 let gameFrame = 0;
 const staggerFrame = 5;
 const spriteAnimations = [];
@@ -21,6 +23,38 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'confused',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'knockout',
+        frames: 12,
+    },
+    {
+        name: 'hit',
+        frames: 4,
     }
 ];
 animationStates.forEach((state, index) => {
@@ -39,10 +73,10 @@ console.log(animationStates)
 // Animation for sprite using the canvas and sprite sizes
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimations['standing'].loc.length;
+    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimations[playerState].loc.length;
     let frameX = spriteWidth * position;
-    let frameY = spriteAnimations['standing'].loc[position].y;
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     gameFrame++;
     requestAnimationFrame(animate);
