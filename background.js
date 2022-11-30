ac949f18787fc21f43a38bccd2283814144b2a07
Amor-Jansen <Amor.jansen2@gmail.com>
2022-11-30 15:16:23 +0000

    Add animation to first layer of background

--- background.js
+++ background.js
@@ -5,16 +5,22 @@ const CANVAS_HEIGHT = canvas.width = 700;
 let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'assets/images/1.png'
+backgroundLayer1.src = 'assets/images/1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'assets/images/2.png'
+backgroundLayer2.src = 'assets/images/2.png';
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'assets/images/3.png'
+backgroundLayer3.src = 'assets/images/3.png';
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'assets/images/4.png'
+backgroundLayer4.src = 'assets/images/4.png';
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'assets/images/5.png'
+backgroundLayer5.src = 'assets/images/5.png';
 const backgroundLayer6 = new Image();
-backgroundLayer6.src = 'assets/images/6.png'
+backgroundLayer6.src = 'assets/images/6.png';
 const backgroundLayer7 = new Image();
-backgroundLayer7.src = 'assets/images/7.png'
+backgroundLayer7.src = 'assets/images/7.png';
+
+function animate(){
+    ctx.drawImage(backgroundLayer1, 0, 0);
+    requestAnimationFrame(animate);
+};
+animate();
