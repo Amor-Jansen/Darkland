d75d1ae00ef8bfe3342431be6826f990a1d2cc43
Amor-Jansen <Amor.jansen2@gmail.com>
2022-11-30 15:04:51 +0000

    Add background images in layers

--- background.js
+++ background.js
@@ -2,3 +2,19 @@ const canvas = document.getElementById('can2');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.width = 700;
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'assets/images/1.png'
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'assets/images/2.png'
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'assets/images/3.png'
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'assets/images/4.png'
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'assets/images/5.png'
+const backgroundLayer6 = new Image();
+backgroundLayer6.src = 'assets/images/6.png'
+const backgroundLayer7 = new Image();
+backgroundLayer7.src = 'assets/images/7.png'
