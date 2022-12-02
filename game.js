e3de690df2c44ab7cd5b737e1066c696c9425d3f
Amor-Jansen <Amor.jansen2@gmail.com>
2022-12-02 23:06:16 +0000

    Set canvas width and height

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 //Created a load eventlistener to ensure images on canvas load before the game starts
 window.addEventListener('load', function(){
     const canvas = document.getElementById('game-canvas');
-    const context = canvas.getContext('2d')
+    const context = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
 })
