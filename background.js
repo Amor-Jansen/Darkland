fa772e27a0feae55172cec58958bbe506424ed1e
Amor-Jansen <Amor.jansen2@gmail.com>
2022-12-02 23:20:25 +0000

    Fix background image load

--- background.js
+++ background.js
@@ -7,23 +7,23 @@ let gameSpeed = 5;
 
 //Background Layers for parallax (for the background to move at a different speed to the character)
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = './assets/images/1.png';
+backgroundLayer1.src = 'assets/images/1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = './images/2.png';
+backgroundLayer2.src = 'assets/images/2.png';
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = './images/3.png';
+backgroundLayer3.src = 'assets/images/3.png';
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = './images/4.png';
+backgroundLayer4.src = 'assets/images/4.png';
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = './images/5.png';
+backgroundLayer5.src = 'assets/images/5.png';
 const backgroundLayer6 = new Image();
-backgroundLayer6.src = './images/6.png';
+backgroundLayer6.src = 'assets/images/6.png';
 const backgroundLayer7 = new Image();
-backgroundLayer7.src = './images/7.png';
+backgroundLayer7.src = 'assets/images/7.png';
 
 //Animation of the background
 function animate(){
-    ctx.drawImage(backgroundLayer1, 0, 0);
+    ctx.drawImage(backgroundLayer5, 0, 0);
     requestAnimationFrame(animate);
 };
 animate();
