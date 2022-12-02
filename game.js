8daaf42a0e15856522b0ec43eba745230856c5fe
Amor-Jansen <Amor.jansen2@gmail.com>
2022-12-02 23:04:42 +0000

    Add 2d context to game javascript

---
+++ game.js
@@ -0,0 +1,5 @@
+//Created a load eventlistener to ensure images on canvas load before the game starts
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('game-canvas');
+    const context = canvas.getContext('2d')
+})
