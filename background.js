cf5dae3e0e7818c8ae1524798f4a8f081683832a
Amor-Jansen <Amor.jansen2@gmail.com>
2022-12-04 16:11:20 +0000

    Fix background scrolling effect

--- background.js
+++ background.js
@@ -22,18 +22,18 @@ const backgroundLayer7 = new Image();
 backgroundLayer7.src = 'assets/images/7.png';
 
 let x = 0;
-let x2 = 1000;
+let xx = 1920;
 
 //Animation loop for the background
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(backgroundLayer2, x, 0);
-    ctx.drawImage(backgroundLayer2, x2, 0);
-    if (x < -1000) x = 1000;
+    ctx.drawImage(backgroundLayer2, xx, 0);
+    //seamless scrolling if/else
+    if (x < -1920) x = 1920 - gameSpeed;
     else x -= gameSpeed;
-    if (x2 < -1000) x2 = 1000;
-    else x2 -= gameSpeed;
-    x2 -= gameSpeed;
+    if (xx < -1920) xx = 1920 - gameSpeed;
+    else xx -= gameSpeed;
     requestAnimationFrame(animate);
 };
 animate();
