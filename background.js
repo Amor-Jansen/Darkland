a372661f488095c7f8c816ee9ff8a6d201b74ea6
Amor-Jansen <Amor.jansen2@gmail.com>
2022-12-04 16:19:03 +0000

    Remove unused code in background animation

--- background.js
+++ background.js
@@ -21,19 +21,13 @@ backgroundLayer6.src = 'assets/images/6.png';
 const backgroundLayer7 = new Image();
 backgroundLayer7.src = 'assets/images/7.png';
 
-let x = 0;
-let xx = 1920;
+class layer {
+    
+}
 
 //Animation loop for the background
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer2, x, 0);
-    ctx.drawImage(backgroundLayer2, xx, 0);
-    //seamless scrolling if/else
-    if (x < -1920) x = 1920 - gameSpeed;
-    else x -= gameSpeed;
-    if (xx < -1920) xx = 1920 - gameSpeed;
-    else xx -= gameSpeed;
     requestAnimationFrame(animate);
 };
 animate();
